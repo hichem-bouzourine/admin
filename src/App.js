dddfb5905624c455482b22be92c4228aa2804887
Hichem <hichembouzourine.uni@gmail.com>
2022-08-29 02:46:32 +0100

    Create Sidebar / Material UI.

--- src/App.js
+++ src/App.js
@@ -7,22 +7,24 @@ import New from "./pages/new/New";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/">
-        <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="users">
-          <Route index element={<List />} />
-          <Route path=":userId" element={<Single />} />
-          <Route path="new" element={<New />} />
+    <>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+          <Route path="products">
+            <Route index element={<List />} />
+            <Route path=":productId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
         </Route>
-        <Route path="products">
-          <Route index element={<List />} />
-          <Route path=":productId" element={<Single />} />
-          <Route path="new" element={<New />} />
-        </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </>
   );
 }
 
--- src/components/sideBar/Sidebar.jsx
+++ src/components/sideBar/Sidebar.jsx
@@ -1,7 +1,103 @@
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import GroupIcon from "@mui/icons-material/Group";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import StoreMallDirectoryIcon from "@mui/icons-material/StoreMallDirectory";
+import WysiwygIcon from "@mui/icons-material/Wysiwyg";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSuggestIcon from "@mui/icons-material/SettingsSuggest";
+import ErrorIcon from "@mui/icons-material/Error";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import LogoutIcon from "@mui/icons-material/Logout";
 import "./sidebar.scss";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebarContainer">
+      <div className="top">
+        <h1 className="logo">Admin Dashboard</h1>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <li className="sectionTitle">MAIN</li>
+          <li>
+            <DashboardIcon className="icon" />
+            <span className="iconTitle">Dashboard</span>
+          </li>
+        </ul>
+        <ul>
+          <li className="sectionTitle">LISTS</li>
+          <li>
+            <GroupIcon className="icon" />
+            <span className="iconTitle">Users</span>
+          </li>
+          <li>
+            <StoreMallDirectoryIcon className="icon" />
+            <span className="iconTitle">Products</span>
+          </li>
+          <li>
+            <WysiwygIcon className="icon" />
+            <span className="iconTitle">Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span className="iconTitle">Delivery</span>
+          </li>
+        </ul>
+
+        <ul>
+          <li className="sectionTitle">USEFUL</li>
+          <li>
+            <QueryStatsIcon className="icon" />
+            <span className="iconTitle">Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span className="iconTitle">Notifications</span>
+          </li>
+        </ul>
+
+        <ul>
+          <li className="sectionTitle">SERVICE</li>
+          <li>
+            <SettingsSuggestIcon className="icon" />
+            <span className="iconTitle">System Health</span>
+          </li>
+          <li>
+            <ErrorIcon className="icon" />
+            <span className="iconTitle">Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon" />
+            <span className="iconTitle">Settings</span>
+          </li>
+        </ul>
+
+        <ul>
+          <li className="sectionTitle">USER</li>
+          <li>
+            <AccountBoxIcon className="icon" />
+
+            <span className="iconTitle">Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon" />
+            <span className="iconTitle">Logout</span>
+          </li>
+        </ul>
+      </div>
+      <hr />
+      <div className="bottom">
+        <span className="sectionTitle">THEME</span>
+        <div className="themes">
+          <span id="white"></span>
+          <span id="black"></span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
