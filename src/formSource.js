814ead85c3b59d81bc8ba5f8791ba7fa4b9db64f
Hichem <hichembouzourine.uni@gmail.com>
2022-08-30 06:27:59 +0100

    Create the form page.

---
+++ src/formSource.js
@@ -0,0 +1,76 @@
+export const userInputs = [
+  {
+    id: 1,
+    label: "Username",
+    type: "text",
+    placeholder: "Hichem",
+  },
+  {
+    id: 2,
+    label: "Name and surname",
+    type: "text",
+    placeholder: "BOUZOURINE Hichem",
+  },
+  {
+    id: 3,
+    label: "Email",
+    type: "mail",
+    placeholder: "Hichembouzourine.uni@gmail.com",
+  },
+  {
+    id: 4,
+    label: "Phone",
+    type: "text",
+    placeholder: "+213 699 092 353",
+  },
+  {
+    id: 5,
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: 6,
+    label: "Address",
+    type: "text",
+    placeholder: "Ouled Yaich, Blida, Algeria",
+  },
+  {
+    id: 7,
+    label: "Country",
+    type: "text",
+    placeholder: "DZA",
+  },
+];
+
+export const productInputs = [
+  {
+    id: 1,
+    label: "Title",
+    type: "text",
+    placeholder: "Apple Macbook Pro",
+  },
+  {
+    id: 2,
+    label: "Description",
+    type: "text",
+    placeholder: "Description",
+  },
+  {
+    id: 3,
+    label: "Category",
+    type: "text",
+    placeholder: "Computers",
+  },
+  {
+    id: 4,
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+  {
+    id: 5,
+    label: "Stock",
+    type: "text",
+    placeholder: "in stock",
+  },
+];
