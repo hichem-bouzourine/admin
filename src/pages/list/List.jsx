ac11306c7878601821a140f247c2c077f06dd1c6
Hichem <hichembouzourine.uni@gmail.com>
2022-08-30 02:32:31 +0100

    Create Users datatable.

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,18 @@
 import "./list.scss";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sideBar/Sidebar";
+import Datatable from "../../components/datatable/Datatable";
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
+  );
 };
 
 export default List;
